effddce77c6f3f4f5604c4b55fd2cfe1c275a91c
Mark Mariscal <markamariscal@gmail.com>
2019-12-04 19:34:20 -0800

    aim camera before rendering part in app.js

--- shadow-mapping/app.js
+++ shadow-mapping/app.js
@@ -230,7 +230,7 @@ function updateAndRender() {
     // Todo - Set up a camera that points in the direction of the light at a
     // reasonably close position such that the scene will be in the view volume.
     // We will set up the view volume boundaries with an orthographics projection.
-    // lightCamera.cameraWorldMatrix.setLookAt(?, ?, ?);
+    lightCamera.cameraWorldMatrix.setLookAt(eyePos,targetPos, worldUp);
 
     camera.update(time.deltaTime);
 
